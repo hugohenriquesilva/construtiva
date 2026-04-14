2e8f16cb6defbf4f39058845cb87c3e9a338ef68
Ca1queGabriel <caiquegabrielgomeslima@gmail.com>
2026-04-13 22:57:24 -0300

    update firebase configuration and integrate analytics

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAGUVb4jbRHLcd0fyWEiKIfWuH8Jjbrd1E",
+  authDomain: "construtiva-c292f.firebaseapp.com",
+  projectId: "construtiva-c292f",
+  storageBucket: "construtiva-c292f.firebasestorage.app",
+  messagingSenderId: "980472634910",
+  appId: "1:980472634910:web:694e2fd530ba16a217180f",
+  measurementId: "G-M02LYKY5N4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
 export default app;
